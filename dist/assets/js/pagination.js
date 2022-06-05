fefed78b4f72fab310387823f086dc6b327bfb5a
Efrain Gonzalez <elandaverdeg@gmail.com>
2022-06-04 23:09:23 -0600

    Added lazyload, videos on homepage and fixed pagination issues

--- dist/assets/js/pagination.js
+++ dist/assets/js/pagination.js
@@ -1,14 +1,60 @@
 var total_records;
-var perpage = 15;
+var perpage = 6;
 var total_pages;
 
 $(document).ready(function(){
 	var pagenum = 1;
+    
     createpagination(pagenum);
 	fetch_data(perpage,pagenum);
-  
+    $(document).on('click','.totop',function(event) {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+     });
 
 });
+
+function createpagination(pagenum){
+    $.ajax({
+        type:'get',
+        url:'https://wankasalud.com/admin/api/video_data',
+        dataType:'json',
+        success:function(data){
+            
+            total_records = data.datavideos.total;
+            total_pages = Math.round(total_records / perpage);
+             $("#page_container").html("");
+    
+    if(pagenum == 1){
+        $("#page_container").append("<li class='page-item disabled previous'><a href='javascript:void(0)' class='page-link'><</a></li>");
+    }else{
+        $("#page_container").append("<li class='page-item totop' onclick='makecall("+(pagenum-1)+")'><a href='javascript:void(0)' class='page-link'><</a></li>");
+    }
+    
+    var i=0;
+    for(i=0; i <= 2; i++){
+        if(pagenum == (pagenum+i)){
+            $("#page_container").append("<li class='page-item disabled'><a href='javascript:void(0)' class='page-link'>"+(pagenum+i)+"</a></li>");
+        }else{
+            if((pagenum+i)<=total_pages){
+                $("#page_container").append("<li class='page-item totop' onclick='makecall("+(pagenum+i)+")'><a href='javascript:void(0)' class='page-link'>"+(pagenum+i)+"</a></li>");					
+            }
+        }
+    }
+    
+    if(pagenum == total_pages){
+        $("#page_container").append("<li class='page-item disabled'><a href='javascript:void(0)' class='page-link'>></a></li>");
+    }else{
+        $("#page_container").append("<li class='page-item next totop' onclick='makecall("+(pagenum+1)+")'><a href='javascript:void(0)' class='page-link'>></a></li>");
+    }
+        },error:function(){
+                $(".100_list_container").html("error");
+            }
+        });
+    
+}
+
+
 function fetch_data(perpage, pagenum){
 	
     $.ajax({
@@ -21,7 +67,7 @@ function fetch_data(perpage, pagenum){
             $.each( data.datavideos.data, function( key, value ) {
                var id_yt=  YouTubeGetID(value.video_url);
                var dateFormatString = convertDate(value.created_at);
-                $(".100_list_container").append("<div class='col-lg-4 mb-5' data-aos='fade-up' data-aos-delay='100'><div class='embed-container'><iframe id='player' type='text/html' width='640' height='360' src='https://www.youtube.com/embed/"+ id_yt +"?enablejsapi=1&origin=http://wankasalud.com/' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div><a href=''><h5 class='mb-1 mt-3'>"+value.title+"</h5><div class='text-small text-muted'>"+dateFormatString+"</div></a></div>");
+                $(".100_list_container").append("<div class='col-lg-4 mb-5' data-aos='fade-up' data-aos-delay='100'><a href='https://www.youtube.com/embed/"+ id_yt +"?autoplay=1' data-fancybox data-type='iframe' class='d-block p-0 border-0'><div class='embed-container'><img class='embed-responsive-item' src='https://img.youtube.com/vi/" +id_yt +"/sddefault.jpg' alt='"+ value.title +"'><img class='icon icon-video-play' src='assets/img/icons/theme/media/dvd.svg' alt='decoration' data-inject-svg /></div><h5 class='mb-1 mt-3'>"+value.title+"</h5><div class='text-small text-muted'>"+dateFormatString+"</div></a></div>");
             });
         },
         error:function(){
@@ -30,63 +76,11 @@ function fetch_data(perpage, pagenum){
     });
 }
 
-
-
-
-function createpagination(pagenum){
-    $.ajax({
-        type:'get',
-        url:'https://wankasalud.com/admin/api/video_data',
-        dataType:'json',
-        success:function(data){
-            
-            total_records = data.datavideos.total;
-            console.log(data.datavideos)
-            console.log(total_records);
-            total_pages = total_records / perpage;
-            console.log(total_pages);
-
-         
-            $("#page_container").html("");
-      
-		
-		if(pagenum == 1){
-			$("#page_container").append("<li class='page-item disabled previous'><a href='javascript:void(0)' class='page-link'><</a></li>");
-		}else{
-			$("#page_container").append("<li class='page-item' onclick='makecall("+(pagenum-1)+")'><a href='javascript:void(0)' class='page-link'><</a></li>");
-		}
-		
-		var i=0;
-		for(i=0; i <= 2; i++){
-
-			if(pagenum == (pagenum+i)){
-				$("#page_container").append("<li class='page-item disabled'><a href='javascript:void(0)' class='page-link'>"+(pagenum+i)+"</a></li>");
-			}else{
-				if((pagenum+i)<=total_pages){
-                    $("#page_container").append("<li class='page-item' onclick='makecall("+(pagenum+i)+")'><a href='javascript:void(0)' class='page-link'>"+(pagenum+i)+"</a></li>");					
-				}
-			}
-		}
-		
-		if(pagenum == total_pages){
-			$("#page_container").append("<li class='page-item disabled'><a href='javascript:void(0)' class='page-link'>></a></li>");
-		}else{
-			$("#page_container").append("<li class='page-item next' onclick='makecall("+(pagenum+1)+")'><a href='javascript:void(0)' class='page-link'>></a></li>");
-		}
-
-        
-		
-        },
-        error:function(){
-            $(".100_list_container").html("error");
-        }
-    });
-		
-}
 function makecall(pagenum){
 	createpagination(pagenum);
     fetch_data(perpage,pagenum);
 }
+
 function YouTubeGetID(url){
     url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
     return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_\-]/i)[0] : url[0];
@@ -105,14 +99,4 @@ if (month < 10) {
 
 var fechaconverter =year+'-' + month + '-'+dt;
 return fechaconverter;
-}
-function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
 }
